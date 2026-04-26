dd3034f3ecf2170f475fdcbd1dddffe3813d99f3
HRxGamer <hrishikeshbande@gmail.com>
2026-04-26 20:47:36 +0530

    Create Question.kt

---
+++ app/src/main/java/com/example/jeesystem/data/Question.kt
@@ -0,0 +1,11 @@
+package com.example.jeesystem.data
+
+data class Question(
+    val subject: String,
+    val topic: String,
+    val subtopic: String,
+    val difficulty: String,
+    val type: String,
+    val question: String,
+    val answer: String
+)
