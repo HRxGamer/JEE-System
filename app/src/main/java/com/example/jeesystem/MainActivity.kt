d0cff9881023020b5c6dc5acb9a065cf0c9f0144
HRxGamer <hrishikeshbande@gmail.com>
2026-04-28 08:30:00 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/jeesystem/MainActivity.kt
+++ app/src/main/java/com/example/jeesystem/MainActivity.kt
@@ -1,8 +1,22 @@
 package com.example.jeesystem
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.jeesystem.databinding.ActivityMainBinding
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Example click (prevents unused warning + good test)
+        binding.btnMath.setOnClickListener {
+            // TODO: Start QuizActivity later
+        }
     }
 }
