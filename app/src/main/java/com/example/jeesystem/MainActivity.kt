fb30592331f9d3df7fbcbf5d02604d4dfcf3866f
HRxGamer <hrishikeshbande@gmail.com>
2026-04-28 09:14:01 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/jeesystem/MainActivity.kt
+++ app/src/main/java/com/example/jeesystem/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.jeesystem
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +10,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnMath = findViewById<Button>(R.id.btnMath)
+
+        btnMath.setOnClickListener {
+            val intent = Intent(this, QuizActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
